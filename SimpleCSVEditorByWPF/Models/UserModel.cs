49468e0eb381078881720ca1736794619a0436ac
kurokoge396 <396robato@gmail.com>
2026-03-26 12:54:45 +0900

    View、ViewModel、Modelフォルダ作成 再配置した

---
+++ SimpleCSVEditorByWPF/Models/UserModel.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper.Configuration;
+
+namespace SimpleCSVEditorByWPF.Models
+{
+    /// <summary>
+    /// ユーザーモデル
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        //[Index(0)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        //[Index(1)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        //[Index(2)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 削除フラグ
+        /// </summary>
+        //[Index(3)]
+        public int IsDeleted { get; set; }
+    }
+
+    /// <summary>
+    /// ユーザーモデルのマッピングクラス
+    /// </summary>
+    public class UserModelMap : ClassMap<UserModel>
+    {
+        public UserModelMap()
+        {
+            Map(m => m.Id).Index(0);
+            Map(m => m.Name).Index(1);
+            Map(m => m.Password).Index(2);
+            Map(m => m.IsDeleted).Index(3);
+        }
+    }
+}
---
+++ SimpleCSVEditorByWPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace SimpleCSVEditorByWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ファイル選択ボタンのクリックイベントハンドラー
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var path = SelectFilePath();
+            if (!string.IsNullOrEmpty(path))
+            {
+                CsvFilePathTextBox.Text = path;
+            }
+        }
+
+        /// <summary>
+        /// ファイル選択ダイアログを表示する
+        /// </summary>
+        /// <returns>ファイルパス</returns>
+        public string SelectFilePath()
+        {
+            // ダイアログのインスタンスを生成
+            var openFileDialog = new OpenFileDialog();
+
+            // フィルタの設定（例：テキストファイルとすべてのファイル）
+            openFileDialog.Filter = "CSVファイル (*.csv)|*.csv";
+
+            // ダイアログを表示し、結果が「OK」の場合のみパスを返す
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName;
+            }
+
+            // キャンセルされた場合は空文字（またはnull）を返す
+            return string.Empty;
+        }
+
+        public void LoadCsvData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+
+            }
+
+
+        }
+    }
+}
